3b6164a8368ecce610334666ab90c4f357d48baf
lavanya0218 <lavanyapandurang18@gmail.com>
2024-07-11 15:22:00 +0530

    Create RemoveDuplications.java

---
+++ RemoveDuplications.java
@@ -0,0 +1,15 @@
+/*
+Write a program to remove duplicates from Sorted Array and Return K where K is the Last position of distinct element.
+Example 1: 
+
+Input: arr[1,1,2,2,2,3,3]
+Output: arr[1,2,3,_,_,_,_]
+ */
+
+public class RemoveDuplications {
+    public static void main(String[] args) {
+        System.out.println("gdksjvbkj");
+    }
+
+    
+}
