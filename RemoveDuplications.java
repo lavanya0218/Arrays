f9e4ccbeee9a0aa767b281e82e744a721914383d
lavanya0218 <lavanyapandurang18@gmail.com>
2024-07-11 19:46:56 +0530

    Update RemoveDuplications.java

--- RemoveDuplications.java
+++ RemoveDuplications.java
@@ -8,8 +8,24 @@ Output: arr[1,2,3,_,_,_,_]
 
 public class RemoveDuplications {
     public static void main(String[] args) {
-        System.out.println("gdksjvbkj");
-    }
+        int arr[] = {1,1,2,2,2,3,3};
+        int k = RemoveDupli(arr);
+        for(int i=0;i<k;i++){
+            System.out.print(arr[i]+" ");
+        }
+        for (int i = k; i < arr.length; i++) {
+            System.out.print("_ ");
+        }
 
-    
+    }   
+    public static int RemoveDupli(int nums[]){
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
 }
